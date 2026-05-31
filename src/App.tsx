14771e6301889f668227778fb701600f38dfd034
inlabcommunication <inlab.communication@gmail.com>
2026-05-31 20:05:23 +0200

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -573,7 +573,7 @@ const PageHome = () => {
         onSecondaryCta={() => go("/portfolio")}
       />
 
-      {{/* Marquee servizi */}
+      {/* Marquee servizi */}
       <MarqueeHome />
 
       {/* MANIFESTO */}
